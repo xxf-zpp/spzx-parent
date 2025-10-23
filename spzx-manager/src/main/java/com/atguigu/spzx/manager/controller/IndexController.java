437631854b594e82e50402ea18d3b27aca39f8fa
xu <2265035472@qq.com>
2025-10-23 22:29:33 +0800

    初始工程提交

---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.atguigu.spzx.manager.controller;
+
+import org.junit.jupiter.api.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag("用户接口")
+@RestController
+@RequestMapping("/admin/system/index")
+public class IndexController {
+
+
+
+}
---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.atguigu.spzx.manager.service.impl;
+
+import com.atguigu.spzx.manager.service.SysUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SysUserServiceImpl implements SysUserService {
+}
